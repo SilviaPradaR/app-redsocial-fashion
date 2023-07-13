a6ff55ecae152d25094e125033f4ec24e05d5b9f
Fernanda Dimitri <dimitri-dg@hotmail.com>
2023-07-13 10:02:57 -0300

    Construcción app y configuración base de datos

---
+++ fashionnet/src/main/java/com/egg/fashionnet/FashionnetApplication.java
@@ -0,0 +1,13 @@
+package com.egg.fashionnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionnetApplication.class, args);
+	}
+
+}
---
+++ fashionnet/src/test/java/com/egg/fashionnet/FashionnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.fashionnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
