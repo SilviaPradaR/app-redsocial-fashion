1cb5004a4eebb445a92ec79e649418afe1a14df5
Ana Fernandez <anny.fdz@gmail.com>
2023-07-14 13:31:03 -0300

    relacion like - usuario onotoone

--- backend/src/main/java/com/egg/backend/entidades/Like.java
+++ backend/src/main/java/com/egg/backend/entidades/Like.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import lombok.AccessLevel;
@@ -33,7 +34,7 @@ public class Like {
     private String idLike;   
     @Temporal(TemporalType.DATE)
     private Date fechaLike;
-    @ManyToOne
+    @OneToOne
     private Usuario usuario;
 //    @ManyToOne
 //    private Publicacion publicacion;
