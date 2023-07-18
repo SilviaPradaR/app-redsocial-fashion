eec240023cac4f8939e3dde172bb2e811cbd4720
Jimena Zaragoza <jimenus@hotmail.com>
2023-07-18 14:59:49 -0300

    Agregado de anotación @repository en Comentario y Reporte

--- backend/src/main/java/com/egg/backend/repositorios/ComentarioRepositorio.java
+++ backend/src/main/java/com/egg/backend/repositorios/ComentarioRepositorio.java
@@ -3,7 +3,9 @@ package com.egg.backend.repositorios;
 
 import com.egg.backend.entidades.Comentario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ComentarioRepositorio extends JpaRepository<Comentario, String> {
     
 }
--- backend/src/main/java/com/egg/backend/repositorios/ReporteRepositorio.java
+++ backend/src/main/java/com/egg/backend/repositorios/ReporteRepositorio.java
@@ -3,7 +3,9 @@ package com.egg.backend.repositorios;
 
 import com.egg.backend.entidades.Reporte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReporteRepositorio extends JpaRepository<Reporte, String>{
     
 }
