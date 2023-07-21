e3acecd8c214c8fb48f4944135e02cd0d271ba31
Jimena Zaragoza <jimenus@hotmail.com>
2023-07-21 18:39:21 -0300

    Carga de ComentarioServicio y agregación de anotación @Repository a UsuarioRepositorio y LikeRepositorio

--- backend/src/main/java/com/egg/backend/repositorios/LikeRepositorio.java
+++ backend/src/main/java/com/egg/backend/repositorios/LikeRepositorio.java
@@ -4,7 +4,9 @@ package com.egg.backend.repositorios;
 
 import com.egg.backend.entidades.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LikeRepositorio extends JpaRepository<Like, String>{
     
 }
