284d46b2a1bd07bd3acbced7bc36a45a69b2dff7
“Nahuel Motos“ <nahuelalejomotosm@gmail.com>
2023-07-23 17:53:24 -0300

    Cattegoria Servicio completa

---
+++ backend/src/main/java/com/egg/backend/servicios/CategoriaServicio.java
@@ -0,0 +1,78 @@
+
+package com.egg.backend.servicios;
+
+import com.egg.backend.entidades.Categoria;
+import com.egg.backend.excepciones.MiException;
+import com.egg.backend.repositorios.CategoriaRepositorio;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaServicio {
+    
+    
+    @Autowired
+    private CategoriaRepositorio categoriaRepositorio;
+    
+    @Transactional
+    public void crearCategoria(String nombre) throws MiException{
+        
+        validar(nombre);
+        
+        Categoria categoria = new Categoria();
+        
+        categoria.setNombre(nombre);
+        
+        categoriaRepositorio.save(categoria);
+        
+    }
+    
+    @Transactional
+    public void eliminarCategoria(String id) throws MiException {
+        
+        Categoria categoria = categoriaRepositorio.getById(id);
+        categoriaRepositorio.delete(categoria);
+        
+    }
+    
+    public void validar (String nombre) throws MiException{
+        
+        if (nombre == null || nombre.isEmpty()) {
+            throw new MiException ("La categoria no puede ser nula o estar vacia");
+        }    
+    }
+    
+    @Transactional
+    public void modificarCategoria(String id, String nombre) throws MiException{
+        
+        validar(nombre);
+        
+        Optional <Categoria> respuesta= categoriaRepositorio.findById(id);
+        
+        if (respuesta.isPresent()) {
+            
+            Categoria categoria = respuesta.get();
+            
+            categoria.setNombre(nombre);
+            
+            categoriaRepositorio.save(categoria);
+        }
+    }
+    
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    public List<Categoria> listarCategoria() {
+
+        List<Categoria> categoria = new ArrayList();
+
+        categoria = categoriaRepositorio.findAll();
+
+        return categoria;
+    }
+    
+}
+
+
